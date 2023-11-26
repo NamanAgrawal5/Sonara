a9d83ade43635ceb600da3b01e99f8c911960afc
Naman <namanagarwal578@gmail.com>
2023-11-26 22:20:31 +0530

    Added your songs page

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
@@ -8,6 +9,7 @@ import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import useOnPlay from "@/hooks/useOnPlay";
 import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { Router } from "next/router";
 
 interface LibraryProps {
   songs: Song[];
@@ -15,6 +17,7 @@ interface LibraryProps {
 const Library: React.FC<LibraryProps> = ({songs}) => {
   const SubsribeModal = useSubscribeModal();
   const authModal = useAuthModal();
+  const router = useRouter();
   const uploadModal = useUploadModal();
   const { user,subscription } = useUser();
   const onPlay = useOnPlay(songs);
@@ -41,10 +44,13 @@ const Library: React.FC<LibraryProps> = ({songs}) => {
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
-        {songs.map((item) => (
+        {songs.slice(0,3).map((item) => (
           <MediaItem onClick={(id:string)=>onPlay(id)} key ={item.id} data={item} />
         ))}
       </div>
+      <button onClick={()=> router.push("/songs")} className="ml-auto text-neutral-400 cursor-pointer hover:text-[#E0FBFC] transition mr-5">
+      <span>See More</span>
+    </button>
     </div>
   );
 };
